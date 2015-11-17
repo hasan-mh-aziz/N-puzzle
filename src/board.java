473ff8a408e290af061f06b958e771029c2c1e1b
hasan-mh-aziz <hasan.mh.aziz@gmail.com>
2015-11-17 12:49:29 +0600

    comment added

--- src/board.java
+++ src/board.java
@@ -1,49 +1,51 @@
 
 import java.util*;
 
+//emni
+
 public class Board {
  public Board(int[ ][] blocks){
-	 
-	 
+
+
  }  // construct a board from an N-by-N
 //array of blocks
  // (where blocks[i][j] = block in
 //row i, column j)
  public int size; // board size N
  public int hamming(){
-	 
+
  }  // number of blocks out of place
- 
+
  public int manhattan(){
-	 
+
  } // sum of Manhattan distances between blocks and goal
  public boolean isGoal(){
-	 
+
  } // is this board the goal board?
  public boolean isSolvable(){
-	 
+
  }  // is the board solvable?
  public boolean equals(Object y){
-	 
+
  } // does this board equal y?
  public Iterable<Board> neighbors(){
-	 
+
  } // all neighboring boards
  public String toString(){
-	 
+
  } // string representation of the
 //board (in the output format specified below)
  public static void main(String[] args){
-	 
-	 
+
+
 	 Scanner in = new Scanner(System.in);
 	 int N;
 	 N=in.nextInt();
-	 
+
 	 board b(N);
 	 b.size=N;
-	 
-	 
+
+
  } // unit test
 }
 
